f7c0b6e0e3b3db85c2b196238540ce41c2fd0c41
alemambo <alexandros.mathios@outlook.com>
2023-10-04 22:23:40 +0100

    frequencies exercise

--- frequencies.py
+++ frequencies.py
@@ -6,12 +6,10 @@
  
 def frequencies(items):
     frequencies = {}
-    count_items=0
+    
     # Your code goes here
     for item in items:
-        count_items = 0
-        count_items = items.count(item)
         key=str(item)
-        frequencies[key]= count_items
+        frequencies[key]= frequencies.get(key,0)+1
     return frequencies
 
