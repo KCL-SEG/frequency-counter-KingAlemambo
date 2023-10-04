0d399749d627530325af8a27d80fd8bae3a3e03f
alemambo <alexandros.mathios@outlook.com>
2023-10-04 22:11:44 +0100

    frequencies exercise

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,17 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+
+ 
 def frequencies(items):
     frequencies = {}
+    count_items=0
     # Your code goes here
+    for item in items:
+        count_items = 0
+        count_items = items.count(item)
+        key=str(item)
+        frequencies[key]= count_items
     return frequencies
+
